4f09b276714885075fb14d969f87fea9730b2f13
jileiahaha <39719268+jileiahaha@users.noreply.github.com>
2018-11-08 08:55:16 +0800

    飞机大战的游戏
    还在编写当中

---
+++ game/alien_invasion.py
@@ -0,0 +1,34 @@
+import pygame
+
+from settings import Settings
+from pygame.sprite import Group
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+    # 初始化pygame、设置和屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # 创建一艘飞船
+    ship = Ship(ai_settings, screen)
+
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
+
+    # 创建一个外星人编组
+    aliens = Group()
+
+    # 创建外星人群
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    while True:
+        # 监视键盘和鼠标事件
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(aliens, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+run_game()
